8841e17dcdba293394116643ecd53036afec98f3
Sheila Kerber <sheila.skerber@gmail.com>
2021-03-09 12:52:02 -0300

    creates file "todosData" (simulates a DB)

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1, 
+        text: "Buy fruits",
+        completed: true
+    },
+    {
+        id: 2, 
+        text: "Water the plants",
+        completed: true
+    },
+    {
+        id: 3, 
+        text: "Call the office",
+        completed: true
+    },
+    {
+        id: 4, 
+        text: "Send Ana's gift",
+        completed: true
+    },
+    {
+        id: 5, 
+        text: "Plann weekend trip",
+        completed: true
+    }
+]
+
+export default todosData
